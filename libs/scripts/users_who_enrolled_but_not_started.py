77a5a1af8a3b8b65962ab656911ad9353a3910a9
IgorNikiforov <igor.nikiforovv@gmail.com>
2019-10-23 12:59:00 +0300

    add figure for distribution of enrolled but not started education users

--- libs/scripts/users_who_enrolled_but_not_started.py
+++ libs/scripts/users_who_enrolled_but_not_started.py
@@ -1,16 +1,18 @@
 import sys
 import datetime
 from tabulate import tabulate
+import plotly.graph_objects as go
 from database_services import *
 
 
 def calculate_users_who_enrolled_but_not_started(connection):
     print('Start query execution at ', datetime.datetime.now())
 
-    get_enrolled_but_not_started_users_query = '''select notStartedUsers.enrolled_but_not_started as user_id, userNames.user_name as user_name from (
-            select enrolledUsers.user_id as enrolled_but_not_started from (
+    get_enrolled_but_not_started_users_query = '''select notStartedUsers.enrolled_but_not_started as user_id, userNames.user_name as user_name, notStartedUsers.enrollment_date as enrollment_date from (
+            select enrolledUsers.user_id as enrolled_but_not_started, enrolledUsers.enrollment_date as enrollment_date from (
                 select   
-                    log_line #>> '{event, user_id}' as user_id
+                    log_line #>> '{event, user_id}' as user_id,
+                    TO_DATE(log_line ->> 'time', 'YYYY-MM-DD"T"HH24:MI:SS')::DATE as enrollment_date
                 from logs
                 where log_line #>> '{event_type}' = 'edx.course.enrollment.activated'		
             ) enrolledUsers
@@ -51,7 +53,7 @@ def calculate_users_who_enrolled_but_not_started(connection):
             ON uniqueUserIds.user_id = userAndIDs.user_id
         ) userNames
         ON userNames.user_id = notStartedUsers.enrolled_but_not_started
-        group by user_name, notStartedUsers.enrolled_but_not_started        
+        group by user_name, notStartedUsers.enrolled_but_not_started, enrollment_date       
         order by user_name desc NULLS LAST'''
 
     connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
@@ -69,7 +71,61 @@ def calculate_users_who_enrolled_but_not_started(connection):
 def write_result_to_file(result_file, result):
     print('Start writing the data to file.')
     with open(result_file,"w") as file:
-        file.write(tabulate(result, headers=['user_id', 'user_name']))
+        file.write(tabulate(result, headers=['user_id', 'user_name', 'enrollment_date']))
+
+
+def generate_figure(enrolled_but_not_started_users):
+    print("Start figures generation...")
+
+    # for every date calculate count
+    date_count = dict()
+    for user in enrolled_but_not_started_users:
+        if user[1]:
+            count_for_date = date_count.get(user[2])
+            if not count_for_date:
+                date_count[user[2]] = 1
+            else:
+                count_for_date += 1
+                date_count[user[2]] = count_for_date
+
+    total_user_count = 0
+    x_axis = []
+    y_axis = []
+    for key in sorted(date_count.keys()):
+        total_user_count += date_count[key]
+        x_axis.append(key)
+        y_axis.append(date_count[key])
+
+    fig = go.Figure(data=go.Scatter(x=x_axis, y=y_axis))
+
+    fig.update_layout(
+        height=500,
+        width=2000,
+        title_text="Distribution of users, who enrolled, but not started the course, "
+                   "depending on the enrolment date. Total users: " + str(total_user_count),
+        xaxis=go.layout.XAxis(
+            title=go.layout.xaxis.Title(
+                text="User enrolment date",
+                font=dict(
+                    family="Courier New, monospace",
+                    size=18,
+                    color="#7f7f7f"
+                )
+            )
+        ),
+        yaxis=go.layout.YAxis(
+            title=go.layout.yaxis.Title(
+                text="Amount of enrolments",
+                font=dict(
+                    family="Courier New, monospace",
+                    size=18,
+                    color="#7f7f7f"
+                )
+            )
+        ))
+
+    print("Opening browser...")
+    fig.show()
 
 
 def main(argv):
@@ -84,6 +140,7 @@ def main(argv):
     connection = open_db_connection(database_name, user_name)
     enrolled_but_not_started_users = calculate_users_who_enrolled_but_not_started(connection)
     write_result_to_file(result_file, enrolled_but_not_started_users)
+    generate_figure(enrolled_but_not_started_users)
     print('The analytics result can be found at ', result_file)
     close_db_connection(connection)
 
