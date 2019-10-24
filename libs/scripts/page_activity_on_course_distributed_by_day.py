99ea0d2ca559b012ea7230ee3bc7c0fdb06e5402
IgorNikiforov <igor.nikiforovv@gmail.com>
2019-10-24 11:41:57 +0300

    Add URL mapping to section names over existing analytics tasks

--- libs/scripts/page_activity_on_course_distributed_by_day.py
+++ libs/scripts/page_activity_on_course_distributed_by_day.py
@@ -1,6 +1,7 @@
 import sys
 import datetime
 import pandas as pd
+import re
 from urllib.parse import unquote
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
@@ -30,6 +31,45 @@ def calculate_page_activity_per_day(connection):
     return page_activity_per_day
 
 
+def calculate_urls_and_names_mapping(connection):
+    print('Start query execution at ', datetime.datetime.now())
+
+    get_urls_and_names_mapping_query = '''select uniqueUrls.target_url as target_url, urlsAndIDs.target_name as target_name from (
+            select 
+                url_decode((log_line ->> 'event')::json ->> 'target_url') as target_url
+            from logs
+			where 
+				log_line ->> 'event_type' LIKE '%link_clicked' or 
+				log_line ->> 'event_type' LIKE '%selected'
+            GROUP BY target_url 
+        ) uniqueUrls
+        LEFT JOIN (
+            select 
+				url_decode((log_line ->> 'event')::json ->> 'target_url') as target_url,
+				(log_line ->> 'event')::json ->> 'target_name' as target_name
+            from logs 
+            where 
+				(log_line ->> 'event_type' LIKE '%link_clicked' or 
+				log_line ->> 'event_type' LIKE '%selected')
+				and (log_line ->> 'event')::json ->> 'target_name' is not null
+            GROUP BY target_name, target_url
+        ) urlsAndIDs
+        ON uniqueUrls.target_url = urlsAndIDs.target_url
+		where uniqueUrls.target_url is not null
+        order by target_name'''
+
+    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor = connection.cursor()
+    cursor.execute(get_urls_and_names_mapping_query)
+    urls_and_names_mapping = cursor.fetchall()
+    cursor.close()
+    connection.commit()
+
+    print('End query execution at ', datetime.datetime.now())
+    print("Urls and names mapping has been calculated")
+    return urls_and_names_mapping
+
+
 def process_urls(result):
     page_activity_per_day_clean = dict()
     for item in result:
@@ -44,6 +84,10 @@ def process_urls(result):
 
         if url.endswith('/'):
             url = url[:-1]
+
+        m = re.search(r'/\d$', url)
+        if m is not None:
+            url = url[:-2]
 
         dates = page_activity_per_day_clean.get(url)
         if not dates:
@@ -61,15 +105,30 @@ def process_urls(result):
     return page_activity_per_day_clean
 
 
-def generate_figure(page_activity_per_day):
+def find_alias(url, urls_and_names_mapping):
+    for url_mapping in urls_and_names_mapping:
+        if url_mapping[0] == url + '/':
+            return url_mapping[1]
+    return None
+
+
+def generate_figure(page_activity_per_day, urls_and_names_mapping):
     print("Start figures generation...")
+
+    titles = []
+    for key in page_activity_per_day.keys():
+        alias = find_alias(key, urls_and_names_mapping)
+        if alias:
+            titles.append(alias + " <br>" + key + "</br>")
+        else:
+            titles.append(key)
 
     pages_length = len(page_activity_per_day)
     row_count = pages_length
     fig = make_subplots(
         rows=row_count,
         cols=1,
-        subplot_titles=(list(page_activity_per_day.keys())))
+        subplot_titles=titles)
 
     count = 1
     for key, value in page_activity_per_day.items():
@@ -106,8 +165,9 @@ def main(argv):
     connection = open_db_connection(database_name, user_name)
     page_activity_per_day = calculate_page_activity_per_day(connection)
     clean_page_activity_per_day = process_urls(page_activity_per_day)
+    urls_and_names_mapping = calculate_urls_and_names_mapping(connection)
     write_result_to_file(result_file, clean_page_activity_per_day)
-    generate_figure(clean_page_activity_per_day)
+    generate_figure(clean_page_activity_per_day, urls_and_names_mapping)
     print('The analytics result can be found at ', result_file)
     close_db_connection(connection)
 
--- libs/scripts/page_activity_on_course_total_number_of_visits.py
+++ libs/scripts/page_activity_on_course_total_number_of_visits.py
@@ -1,5 +1,6 @@
 import sys
 import datetime
+import re
 import pandas as pd
 from urllib.parse import unquote
 import plotly.graph_objects as go
@@ -28,6 +29,45 @@ def calculate_pages(connection):
     return pages_urls
 
 
+def calculate_urls_and_names_mapping(connection):
+    print('Start query execution at ', datetime.datetime.now())
+
+    get_urls_and_names_mapping_query = '''select uniqueUrls.target_url as target_url, urlsAndIDs.target_name as target_name from (
+            select 
+                url_decode((log_line ->> 'event')::json ->> 'target_url') as target_url
+            from logs
+			where 
+				log_line ->> 'event_type' LIKE '%link_clicked' or 
+				log_line ->> 'event_type' LIKE '%selected'
+            GROUP BY target_url 
+        ) uniqueUrls
+        LEFT JOIN (
+            select 
+				url_decode((log_line ->> 'event')::json ->> 'target_url') as target_url,
+				(log_line ->> 'event')::json ->> 'target_name' as target_name
+            from logs 
+            where 
+				(log_line ->> 'event_type' LIKE '%link_clicked' or 
+				log_line ->> 'event_type' LIKE '%selected')
+				and (log_line ->> 'event')::json ->> 'target_name' is not null
+            GROUP BY target_name, target_url
+        ) urlsAndIDs
+        ON uniqueUrls.target_url = urlsAndIDs.target_url
+		where uniqueUrls.target_url is not null
+        order by target_name'''
+
+    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor = connection.cursor()
+    cursor.execute(get_urls_and_names_mapping_query)
+    urls_and_names_mapping = cursor.fetchall()
+    cursor.close()
+    connection.commit()
+
+    print('End query execution at ', datetime.datetime.now())
+    print("Urls and names mapping has been calculated")
+    return urls_and_names_mapping
+
+
 def process_urls(result):
     urls_without_parameters = dict()
     for item in result:
@@ -43,6 +83,10 @@ def process_urls(result):
         if url.endswith('/'):
             url = url[:-1]
 
+        m = re.search(r'/\d$', url)
+        if m is not None:
+            url = url[:-2]
+
         interaction_count = urls_without_parameters.get(url)
         if not interaction_count:
             interaction_count = 0
@@ -52,25 +96,37 @@ def process_urls(result):
     return urls_without_parameters
 
 
-def generate_figure(activity_distribution):
+def find_alias(url, urls_and_names_mapping):
+    for url_mapping in urls_and_names_mapping:
+        if url_mapping[0] == url + '/':
+            return url_mapping[1]
+    return None
+
+
+def generate_figure(activity_distribution, urls_and_names_mapping):
     print("Start figures generation...")
 
     x_axis = []
     y_axis = []
     for key, value in activity_distribution.items():
-        x_axis.append(key)
+        alias = find_alias(key, urls_and_names_mapping)
+        if alias:
+            x_axis.append(alias + " (" + key + ")")
+        else:
+            x_axis.append(key)
         y_axis.append(value)
 
     fig = go.Figure(data=[go.Bar(
-        x=x_axis, y=y_axis,
+        x=y_axis, y=x_axis,
         text=y_axis,
         textposition='auto',
+        orientation='h'
     )])
 
     fig.update_layout(title_text='Amount of interactions with the course pages',
                       autosize=False,
-                      width=20*len(x_axis),
-                      height=1000)
+                      width=3000,
+                      height=20*len(x_axis))
     print("Opening browser...")
     fig.show()
 
@@ -90,8 +146,9 @@ def main(argv):
     connection = open_db_connection(database_name, user_name)
     pages_urls = calculate_pages(connection)
     unique_urls_without_parameters = process_urls(pages_urls)
+    urls_and_names_mapping = calculate_urls_and_names_mapping(connection)
     write_result_to_file(result_file, unique_urls_without_parameters)
-    generate_figure(unique_urls_without_parameters)
+    generate_figure(unique_urls_without_parameters, urls_and_names_mapping)
     print('The analytics result can be found at ', result_file)
     close_db_connection(connection)
 
