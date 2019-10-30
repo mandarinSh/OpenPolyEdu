75e588bc3035144d99d432109ff05e364a89f7a1
Ivan Sysoev <ivanabc97@gmail.com>
2019-10-30 21:32:36 +0300

    py script refactored + results example added

--- libs/scripts/get_video_watching_durations.py
+++ libs/scripts/get_video_watching_durations.py
@@ -1,22 +1,15 @@
 import datetime
-import json
 import sys
-from database_services import *
 
-# Gets datetime object from log 'time' string
-def parse_datetime(log_timestamp):
-    dt = log_timestamp.split("T")
-    date = dt[0].split("-")
-    time = dt[1].split(".")[0].split(":")
-    return datetime.datetime(int(date[0]), int(date[1]), int(date[2]), hour=int(time[0]), minute=int(time[1]), second=int(time[2]))
+from dateutil import parser
+from database_services import *
 
 # Prints results to the result directory
 def print_result(user_times, result_file):
-    log_fh = open(result_file, "w")
-    log_fh.write("username,time(sec)\n")
-    for username in user_times:
-        log_fh.write(username + "," +  str(user_times[username].total_seconds()) + "\n")
-    log_fh.close()
+    with open(result_file, "w") as log_fh:
+        log_fh.write("username,time(sec)\n")
+        for username in user_times:
+            log_fh.write(username + "," + str(user_times[username].total_seconds()) + "\n")
 
 
 # Calculates total-video-watch-time-durations for every user from DB data
@@ -27,6 +20,7 @@ def calculate_times_for_users(play_pause_events):
     # The auxiliary dict for storing current last played time for user (it's needed in processing)
     user_played_times = dict()
 
+    print("Calculating times started")
     for event in play_pause_events:
         event_type = event[0]
         username = event[1]
@@ -35,7 +29,7 @@ def calculate_times_for_users(play_pause_events):
         if event_type != "play_video" and event_type != "pause_video":
             continue
 
-        cur_time = parse_datetime(time)
+        cur_time = parser.parse(time)
     
         if username not in user_times:
             user_times[username] = datetime.timedelta()
@@ -47,14 +41,14 @@ def calculate_times_for_users(play_pause_events):
 
     return user_times
 
-
-# Doing the analytic task (get users' video-watching durations)
-def do_task(connection, result_file):
-
+def get_result_from_db(connection):
     # Get all play and pause events from video_events table	
-    get_play_pause_events_query = ''' SELECT log_line ->> 'event_type' as event_t, log_line -> 'username' as username, log_line -> 'time' as time 
-	    							  FROM logs WHERE log_line ->> 'event_type' = 'pause_video' OR 
-		    						  log_line ->> 'event_type' = 'play_video' '''
+    get_play_pause_events_query = ''' SELECT log_line ->> 'event_type' as event_t, 
+	                                         log_line -> 'username' as username,
+											 log_line -> 'time' as time 
+	    							  FROM logs 
+									  WHERE log_line ->> 'event_type' = 'pause_video' OR 
+		    						        log_line ->> 'event_type' = 'play_video' '''
 
     connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     cursor = connection.cursor()
@@ -63,9 +57,14 @@ def do_task(connection, result_file):
     cursor.close()
     connection.commit()
 	
+    return play_pause_events
+    
+
+# Doing the analytic task (get users' video-watching durations)
+def execute_analytics_task(result_from_db, result_file):
     # Get dict of users and their total video watching time values
     # Time is represented as datetime.timedelta type
-    user_times = calculate_times_for_users(play_pause_events)
+    user_times = calculate_times_for_users(result_from_db)
 
     # Print results of analytics task as table
     print_result(user_times, result_file)
@@ -73,14 +72,15 @@ def do_task(connection, result_file):
 
 
 def main(argv):
-    print("Start calculating users' video watching time durations")
+    print("Analytics task started...")
 
     database_name = argv[1]
     user_name = argv[2]
     result_file = argv[3]
 
     connection = open_db_connection(database_name, user_name)
-    do_task(connection, result_file)
+    result_from_db = get_result_from_db(connection)
+    execute_analytics_task(result_from_db, result_file)
     close_db_connection(connection)
 
 
