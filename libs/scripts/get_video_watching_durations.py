329f9ee8305c269ff4843f681b7d4616e003af14
Ivan Sysoev <ivanabc97@gmail.com>
2019-11-14 15:24:35 +0300

    Video watching times were separated by months within a course

--- libs/scripts/get_video_watching_durations.py
+++ libs/scripts/get_video_watching_durations.py
@@ -5,15 +5,27 @@ from dateutil import parser
 from database_services import *
 
 # Prints results to the result directory
-def print_result(user_times, result_file):
+def print_result(user_times, months, result_file):
     with open(result_file, "w") as log_fh:
-        log_fh.write("username,time(min)\n")
+        log_fh.write("username")
+        for month in months:
+            log_fh.write(",time in " + str(month) + " month")
+        log_fh.write(",total time")
+        log_fh.write("\n")
+
         for user_time in user_times:
-            log_fh.write(user_time[0] + "," + str(user_time[1].total_seconds() // 60) + "\n")
+            log_fh.write(user_time[0])
+            month_durations = user_time[1]
+            for month_duration in month_durations:
+                log_fh.write("," + str(month_duration.total_seconds() // 60))
+            log_fh.write(str(sum(month_durations, datetime.timedelta()).total_seconds() // 60))
+            log_fh.write("\n")
 
 
 # Calculates total-video-watch-time-durations for every user from DB data
-def calculate_times_for_users(play_pause_events):
+def calculate_times_for_users(play_pause_events, months):
+
+    course_duration_in_months = len(months)
 
     # The dict matching username and his total time duration of video watching
     user_times = dict()
@@ -35,7 +47,7 @@ def calculate_times_for_users(play_pause_events):
         cur_time = parser.parse(time)
 
         if username not in user_times:
-            user_times[username] = datetime.timedelta()
+            user_times[username] = [datetime.timedelta()] * course_duration_in_months
             user_played_times[username] = cur_time
         elif event_type == "play_video":
             user_played_times[username] = cur_time
@@ -43,7 +55,7 @@ def calculate_times_for_users(play_pause_events):
         elif session == last_play_video_session:
             # Check that last play_video event was emitted within the same session with current pause_video event
             # (e.g. not in the session some days ago - to avoid incorrect large timedelta values)
-            user_times[username] += cur_time - user_played_times[username]
+            user_times[username][months.index(cur_time.month)] += cur_time - user_played_times[username]
 
     return user_times
 
@@ -66,18 +78,32 @@ def get_result_from_db(connection):
     connection.commit()
 	
     return play_pause_events
-    
 
 # Doing the analytic task (get users' video-watching durations)
 def execute_analytics_task(result_from_db, result_file):
     # Get dict of users and their total video watching time values
     # Time is represented as datetime.timedelta type
-    user_times = calculate_times_for_users(result_from_db)
+
+    first_record_month = parser.parse(result_from_db[0][2]).month
+    last_record_month = parser.parse(result_from_db[-1][2]).month
+
+    # Creating months array for course months
+    i = first_record_month
+    months = []
+    while i != last_record_month:
+        if i > 12:
+            i = 1
+            continue
+        months.append(i)
+        i += 1
+    months.append(last_record_month)
+
+    user_times = calculate_times_for_users(result_from_db, months)
 
     # Sort users by their time video watching descending
-    user_times = sorted(user_times.items(), key=lambda x : x[1], reverse=True)
+    user_times = sorted(user_times.items(), key=lambda x : sum(x[1], datetime.timedelta()), reverse=True)
     # Print results of analytics task as table
-    print_result(user_times, result_file)
+    print_result(user_times, months, result_file)
     print('The analytics result can be found at ', result_file)
 
 
