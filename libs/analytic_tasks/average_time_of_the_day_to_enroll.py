28f569394dad4b343205bd3a439f7954ba8fc19d
AndreyKoltsov1997 <a99920641@gmail.com>
2019-11-14 13:29:57 +0300

    Update docstrings. Remove deprecated functions.

--- libs/analytic_tasks/average_time_of_the_day_to_enroll.py
+++ libs/analytic_tasks/average_time_of_the_day_to_enroll.py
@@ -9,38 +9,20 @@ sys.path.append("../scripts/")
 from database_services import *
 
 
-"""
-* Retrieves unique course IDs from the connected database.
-* @param connection - connection to the target database;
-* @returns list of unique courses' IDs;
-"""
-def get_unique_course_ids(connection):
-    unique_course_id_query = '''
-    SELECT DISTINCT (log_line ->> 'context')::json ->> 'course_id' AS course_identifier 
-    FRO, logs ORDER BY course_identifier
-    '''
-    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    cursor = connection.cursor()
-    cursor.execute(unique_course_id_query)
-    unique_course_ids = cursor.fetchall()
-    cursor.close()
-    connection.commit()
-    return unique_course_ids
-
-
 """ 
 * Fetches distribution of the courses' enrollment.
 * @returns map - course ID and enrollment time. Not that course ID is not distinct.
 """
 def get_enrollment_distribution(connection):
+    # NOTE: The result should generate a table with columns 'id' and 'average time to enroll'
     enrollment_distribution_sql_request = ''' 
     SELECT uniqueCourseIds.identifier as course_identifier, AVG(uniqueCourseIds.target_time) as course_time from (
         SELECT (log_line ->> 'context')::json ->> 'course_id' AS identifier,
         to_timestamp(log_line ->> 'time', 'YYYY-MM-DD"T"HH24:MI:SS')::TIME AS target_time
         FROM logs 
+        WHERE log_line ->> 'event_type' = '.activated' 
     ) uniqueCourseIds GROUP BY course_identifier
     '''
-
     connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     cursor = connection.cursor()
     cursor.execute(enrollment_distribution_sql_request)
@@ -49,56 +31,27 @@ def get_enrollment_distribution(connection):
     connection.commit()
     return enrollment_distribution_course_id_and_time
 
-"""
-* Retrieves average time to enroll any course.
-* @param connection - connection to the target database;
- *@returns average time to enroll any course;
-"""
-def get_average_time_to_enroll_any_course(connection):
-    # NOTE: Getting average time to enroll the course based on every enrolling event.
-    total_average_time_to_enroll_course = '''
-        SELECT enrolling_events.target_time from (
-            SELECT avg(to_timestamp(log_line ->> 'time', 'YYYY-MM-DD"T"HH24:MI:SS')::TIME) as target_time
-            FROM logs 
-            WHERE log_line ->> 'event_type' LIKE '%.activated'
-        ) enrolling_events
-    '''
-    
-    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    cursor = connection.cursor()
-    cursor.execute(total_average_time_to_enroll_course)
-    average_time_to_enroll = cursor.fetchall()
-    cursor.close()
-    connection.commit()
-    return average_time_to_enroll
-
-def generate_heatmap(x_values, y_values, z_values):
-    enrollment_distribution_heatmap = go.Figure(data=go.Heatmap(
-        z=z_values,
-        x=x_values,
-        y=y_values)
-    )
-    enrollment_distribution_heatmap.update_layout(
-        title="Course enrollment statistics",
-        xaxis_nticks=10,
-        xaxis_title="Time, minutes",
-        yaxis_title="Amount of peoople enrolled"
-    )
-
-
-
+def write_result_to_file(result_file, result):
+    print('Start writing the data to file.')
+    with open(result_file, mode='w', encoding='utf-8') as res_file:
+        field_names = ['course_id', 'average time to enroll']
+        result_file_writer = csv.writer(res_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        result_file_writer.writerow(field_names)
+        for res in result:
+            result_file_writer.writerow(res)
 
 def main(argv):
-    print('Start calculating page activity on course distributed by day.')
-
-    # NOTE: Mocking credentials while debugging.
-    database_name = "OpenEduDatabase"
-    user_name = "OPENEDU"
+    print('Start calculating average time to enroll the course.')
+    database_name = argv[1]
+    user_name = argv[2]
+    result_file = argv[3]
 
     connection = open_db_connection(database_name, user_name)
-    average_time_to_enroll_any_course = get_enrollment_distribution(connection)
-    print('Average time of the day to get enrolled into the course is {}'.format(average_time_to_enroll_any_course))
+    enrollment_distribution = get_enrollment_distribution(connection)
     close_db_connection(connection)
+
+    write_result_to_file(result_file, enrollment_distribution)
+    println(f"The result file could be found at ${result_file}")
 
 
 if __name__ == '__main__':
